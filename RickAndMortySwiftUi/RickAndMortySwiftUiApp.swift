400d6cc894e67ee034a84ac6d53444ca9718d3c3
Otto <jiokotok@yandex.ru>
2023-08-27 21:33:14 +0300

    Initial Commit

---
+++ RickAndMortySwiftUi/RickAndMortySwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortySwiftUiApp.swift
+//  RickAndMortySwiftUi
+//
+//  Created by Otto Dzhandzhuliya on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortySwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
